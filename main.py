2cb5e0dc42b5860f34abc0749cdf5e580985047e
GOD <andjeybur@gmail.com>
2019-12-25 22:14:58 +0300

    libs

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import choice, shuffle, random
+from copy import deepcopy
+import sys
+from PyQt5.QtCore import Qt, QTimer, QThread
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QListView
+from PyQt5.QtWidgets import QLineEdit, QMainWindow, QCheckBox, QPlainTextEdit
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QFormLayout, QListWidget, QListWidgetItem, QDialog
+from PyQt5 import uic
+from PyQt5.QtGui import QPalette, QImage, QBrush, QPixmap, QIcon
+
+
+
+
+
+
