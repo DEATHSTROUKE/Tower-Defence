6c216d1421748bf584a31caf8b2e30230acf8253
GOD <andjeybur@gmail.com>
2019-12-25 22:20:15 +0300

    menu1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import choice, shuffle, random
 from copy import deepcopy
 import sys
-from PyQt5.QtCore import Qt, QTimer, QThread
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QListView
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel
 from PyQt5.QtWidgets import QLineEdit, QMainWindow, QCheckBox, QPlainTextEdit
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from PyQt5.QtWidgets import QFormLayout, QListWidget, QListWidgetItem, QDialog
@@ -10,7 +10,16 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPalette, QImage, QBrush, QPixmap, QIcon
 
 
+class Menu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('menu.ui', self)
+        self.setWindowIcon(QIcon('data/Pictures/171.png'))
+        self.setWindowTitle('Tower Defence')
 
 
-
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    n = Menu()
+    n.show()
+    sys.exit(app.exec_())
