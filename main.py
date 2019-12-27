63696caf87ceab506590c18f7b8f7fc9da93dd95
GOD <andjeybur@gmail.com>
2019-12-27 13:32:09 +0300

    levels1

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPalette, QImage, QBrush, QPixmap, QIcon
 import pygame as pg
 
+GRAVITY = 1
+width, height = 500, 500
+size = 500, 500
+
+
+# screen = pg.display.set_mode(size)
+# screen.fill(pg.Color('black'))
+
 
 class Menu(QMainWindow):
     def __init__(self):
@@ -25,7 +33,33 @@ class Menu(QMainWindow):
             n.showFullScreen()
             self.update_bg()
         elif event.key() == Qt.Key_Escape:
-            n.showMaximized()
+            n.showNormal()
+            self.update_bg()
+
+    def update_bg(self):
+        self.palette = QPalette()
+        self.im = QImage('data/grass.svg')
+        self.im = self.im.scaledToWidth(self.width())
+        self.im = self.im.scaledToHeight(self.height())
+        self.palette.setBrush(QPalette.Background, QBrush(self.im))
+        self.setPalette(self.palette)
+
+
+class Levels(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('menu.ui', self)
+        self.setGeometry(300, 100, 1300, 900)
+        self.setWindowIcon(QIcon('data/Pictures/171.png'))
+        self.setWindowTitle('Tower Defence')
+        self.update_bg()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_F11:
+            n.showFullScreen()
+            self.update_bg()
+        elif event.key() == Qt.Key_Escape:
+            n.showNormal()
             self.update_bg()
 
     def update_bg(self):
@@ -40,6 +74,5 @@ class Menu(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     n = Menu()
-    n.showMaximized()
-    n.update_bg()
+    n.show()
     sys.exit(app.exec_())
