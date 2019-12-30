8b7c899828e6af1709810212f0f3308367794d57
GOD <andjeybur@gmail.com>
2019-12-30 16:56:22 +0300

    menu7

--- main.py
+++ main.py
@@ -27,31 +27,15 @@ class Menu(QMainWindow):
         self.setFixedSize(1300, 900)
         self.setWindowIcon(QIcon('data/Pictures/171.png'))
         self.setWindowTitle('Tower Defence')
-        self.update_bg('st')
+        self.update_bg()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_F11:
-            n.showFullScreen()
-            self.update_bg('full')
-        elif event.key() == Qt.Key_Escape:
-            n.showNormal()
-            self.update_bg('st')
-
-    def update_bg(self, size):
+    def update_bg(self):
         self.palette = QPalette()
         self.im = QImage('data/grass.svg')
         self.im = self.im.scaledToWidth(self.width())
         self.im = self.im.scaledToHeight(self.height())
         self.palette.setBrush(QPalette.Background, QBrush(self.im))
         self.setPalette(self.palette)
-        self.play.move(self.width() // 2 - self.play.width() // 2,
-                       self.height() // 2 - self.play.height() // 2)
-        if size == 'full':
-            self.title.setIconSize(QSize(1300, 1000))
-            self.title.resize(self.title.sizeHint())
-        else:
-            self.title.setIconSize(QSize(400, 400))
-            self.title.resize(self.title.sizeHint())
 
 
 class Levels(QMainWindow):
